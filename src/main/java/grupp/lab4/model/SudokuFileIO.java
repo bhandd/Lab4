ccd1fbe2b699cbf70b3a606fd1323d853138eb90
bjorn <bjorn1987@gmail.com>
2023-12-18 15:25:24 +0100

    moved eventSavegameHandler, eventLoadGameHandler, eventExitHandler from GridView to Controller

--- src/main/java/grupp/lab4/model/SudokuFileIO.java
+++ src/main/java/grupp/lab4/model/SudokuFileIO.java
@@ -13,15 +13,13 @@ public class SudokuFileIO {
      */
     //TODO:
     public static void serializeToFile(File file, Bord sudokuBord) throws IOException {
-//ORIGINAL
- ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
-        try {
+        //ORIGINAL
+        //try wtih resources autoclosar outputstream
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));) {
             out.writeObject(sudokuBord);
 
         } catch (IOException IOEx) {
             throw new IOException("Something went wrong when writing to fille");
-        }finally {
-            out.close();
         }
         //TODO: close file
 
@@ -42,14 +40,12 @@ public class SudokuFileIO {
         try {
             in = new ObjectInputStream(new FileInputStream(file));
             return (Bord) in.readObject();
-//        }catch (NullPointerException e) {
-//            throw new NullPointerException("Är det verkligen nödvändigt med en Alert här?"); //throws exeption to calling method
+        }catch (NullPointerException e) {
+            throw new NullPointerException("Är det verkligen nödvändigt med en Alert här?"); //throws exeption to calling method
 
-        } catch (EOFException exception) {
-        throw  new EOFException("Could not read file");
         }
          catch (ClassNotFoundException e) {
-            throw new RuntimeException("Could not read file, file corrupt or wrong type");
+            throw new ClassNotFoundException("Could not read file, file corrupt or wrong type");
         }
         finally {
                 if (in != null)
